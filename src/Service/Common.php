4479ff226e48ebe29cb14f7e151ea0994c972126
lishuqiang <>
2022-12-30 11:45:11 +0800

    第四题

--- src/Service/Common.php
+++ src/Service/Common.php
@@ -10,6 +10,8 @@ namespace App\Service;
 class Common
 { 
     protected static $debug;
+    const RETURN_CODE_SUCCESS   = '200';
+    const RETURN_CODE_FAIL      = '500';
 
     /**
      * geo helper 地址转换为坐标
@@ -20,14 +22,58 @@ class Common
     {
 
         try {
-            $cackeKey = 'cache-address-'.$address;
+            $returnData = '';
+            //1,校验地址是否为空
+            if($address){
+                //2,地址不为空取缓存信息
+                $cackeKey = 'cache-address-'.$address;
 
-            // 從獲取座標
-            $userLocation = redisx()->get($cackeKey);
-            if ($userLocation) {
-                return $userLocation;
+                // 從獲取座標
+                $returnData = redisx()->get($cackeKey);
+                if ($returnData) {
+                    //3,缓存中有值直接返回
+                    return $returnData;
+                }else{
+                    //4,缓存中没有值请求三方接口
+                    $locationByAddress = $this->getLocationByAddress($merchant_id);
+                    if(isset($locationByAddress['status']) == self::RETURN_CODE_SUCCESS){
+                        $returnData = $locationByAddress['data']  ?: '';
+                    }else{
+                        $locationByMId = $this->getLocationByMerchantId($merchant_id);
+                        if(isset($locationByMId['status']) == self::RETURN_CODE_SUCCESS){
+                            $returnData = $locationByMId['data']  ?: '';
+                        }
+                    }
+                }
+            }else{
+                //5,地址为空根据商家ID取
+                $locationByMId = $this->getLocationByMerchantId($merchant_id);
+                if(isset($locationByMId['status']) == self::RETURN_CODE_SUCCESS){
+                    $returnData = $locationByMId['data']  ?: '';
+                }
             }
+            return $returnData;
 
+        } catch (\Throwable $t) {
+            criticalLog('geoHelperAddress critical ==' . $t->getMessage());
+            return 0;
+        }
+    }
+
+    /*
+     * @DESC 根据地址获取坐标
+     * @author lishuqiang
+     * @date 20221230
+     * @param string address
+     * @return array
+     */
+    public static function getLocationByAddress($address)
+    {
+        $returnData['status']   = self::RETURN_CODE_SUCCESS;//返回状态码
+        $returnData['msg']      = ''; //提示信息
+        $returnData['data']     = '';//返回数据信息
+        if(!empty($address)){
+            $cackeKey = 'cache-address-'.$address;
             $key = 'time=' . time();
 
             // requestLog：寫日志
@@ -38,50 +84,80 @@ class Common
             $param = json_encode([[$address, $key]]);
 
             // 調用接口，以地址獲取座標
+            //请求第三方接口传参记录日志
+            responseLog('Backend', 'get location inter params', 'https://geo-helper-hostr.ks-it.co', '200', '0',  json_encode($param));
             $response = $geoHelper->convert_addresses($param);
+            //请求第三方接口返回值记录日志
+            responseLog('Backend', 'get location inter result', 'https://geo-helper-hostr.ks-it.co', '200', '0',  $response);
             $response = json_decode($response, true);
 
-            if ($response['error'] == 0) {
-                responseLog('Backend', 'phpgeohelper\\Geocoding->hksf_addresses', 'https://geo-helper-hostr.ks-it.co', '200', '0',  $response);
-                $data = $response['data'][0];
-                $coordinate = $data['coordinate'];
+            if(empty($response)){
+                $returnData['status']   = self::RETURN_CODE_FAIL;
+                $returnData['msg']      = 'requeset get location inter result empty';
+            }else{
+                if ($response['error'] == 0) {
+                    responseLog('Backend', 'phpgeohelper\\Geocoding->hksf_addresses', 'https://geo-helper-hostr.ks-it.co', '200', '0',  $response);
+                    $data = $response['data'][0];
+                    $coordinate = $data['coordinate'];
 
-                // 如果返回 '-999,-999'，表示調用接口失敗，那麼直接使用商家位置的座標
-                if ($coordinate == '-999,-999') {
-                    infoLog('geoHelper->hksf_addresses change failed === ' . $address);
-                    if ($merchant_id) {
-                        $sMerchant = new Merchant();
-                        $res = $sMerchant->get_merchant_address($merchant_id);
-                        $user_location = $res['latitude'] . ',' . $res['longitude'];
-                        return $user_location;
+                    // 如果返回 '-999,-999'，表示調用接口失敗，那麼直接使用商家位置的座標
+                    if ($coordinate == '-999,-999') {
+                        infoLog('geoHelper->hksf_addresses change failed === ' . $address);
+
+                        $returnData['status']   = self::RETURN_CODE_FAIL;
+                        $returnData['msg']      = 'requeset get location inter result error coordinate：'.$coordinate;
+
                     }
-                    infoLog('geoHelper->hksf_addresses change failed === merchant_id is null' . $merchant_id);
-                    return false;
-                }
-                if (!isset($data['error']) && (strpos($coordinate,',') !== false)) {
-                    $arr = explode(',', $coordinate);
-                    $user_location = $arr[1] . ',' . $arr[0];
+                    if (!isset($data['error']) && (strpos($coordinate,',') !== false)) {
+                        $arr = explode(',', $coordinate);
+                        $user_location = $arr[1] . ',' . $arr[0];
 
-                    // set cache
-                    redisx()->set($cackeKey, $user_location);
-                    return $user_location;
+                        // set cache
+                        redisx()->set($cackeKey, $user_location);
+                        $returnData['data'] = $user_location;
+                    }
+                }else{
+                    $returnData['status']   = self::RETURN_CODE_FAIL;
+                    $returnData['msg']      = 'requeset get location inter result error data：'.$response['error'];
                 }
             }
-            responseLog('Backend', 'phpgeohelper\\Geocoding->hksf_addresses', 'https://geo-helper-hostr.ks-it.co', '401', '401',  $response);
-            return false;
-        } catch (\Throwable $t) {
-            criticalLog('geoHelperAddress critical ==' . $t->getMessage());
-            return 0;
+
+
         }
+        return $returnData;
     }
 
+
+    /*
+     * @DESC 根据商家ID获取坐标
+     * @author lishuqiang
+     * @date 20221230
+     * @param string merchant_id
+     * @return array
+     */
+    public static function getLocationByMerchantId($merchant_id){
+        $returnData['status']   = self::RETURN_CODE_SUCCESS;//返回状态码
+        $returnData['msg']      = ''; //提示信息
+        $returnData['data']     = '';//返回数据信息
+        if ($merchant_id) {
+            $sMerchant = new Merchant();
+            $res = $sMerchant->get_merchant_address($merchant_id);
+            $returnData['data'] = $res['latitude'] . ',' . $res['longitude'];
+        }
+        return $returnData;
+    }
     // 回调状态过滤
     public static function checkStatusCallback($order_id, $status)
     {
-        // 是900 可以回调
-        if ($status == 900) {
-            return 1;
+        //1,先验证参数
+        $returnData['status']   = self::RETURN_CODE_SUCCESS;//返回状态码
+        $returnData['msg']      = ''; //提示信息
+        $returnData['data']     = '';//返回数据信息
+        if(empty($order_id) || empty($status)){
+            $returnData['status']   = self::RETURN_CODE_SUCCESS;
+            $returnData['msg']      = 'can not call back';
         }
+        
         // backend状态为 909 915 916 时 解锁工作单 但不回调
         $code_arr = ['909', '915', '916'];
         if (in_array($status, $code_arr)) {
@@ -90,6 +166,8 @@ class Common
         }
 
         $open_status_arr = ['901' => 1, '902' => 2, '903' => 3];
-        return $order_id.'-'.$open_status_arr[$status];
+        $returnData['data'] = $order_id.'-'.$open_status_arr[$status];
+
+        return $returnData;
     }
 }
